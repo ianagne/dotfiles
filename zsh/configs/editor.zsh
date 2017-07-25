83f17c721d4999763cf51a3d105faf044cf4ddc6
Ian Agne <iagne@tscadvantage.com>
2017-07-24 20:37:22 -0500

    Use nvr as VISUAL editor when in nvim terminal mode

--- zsh/configs/editor.zsh
+++ zsh/configs/editor.zsh
@@ -1,2 +1,6 @@
-export VISUAL=nvim
+if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
+  export VISUAL="nvr -cc split --remote-wait +set 'bufhidden=wipe'"
+else
+  export VISUAL=nvim
+fi
 export EDITOR=$VISUAL
