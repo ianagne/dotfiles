7f6f82730f6ce244aa6a83db74bc1eb5d224d7a7
Ian Agne <iagne@tscadvantage.com>
2017-07-24 20:38:32 -0500

    Fix typo in nvr command

--- zsh/configs/editor.zsh
+++ zsh/configs/editor.zsh
@@ -1,5 +1,5 @@
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-  export VISUAL="nvr -cc split --remote-wait +set 'bufhidden=wipe'"
+  export VISUAL="nvr -cc split --remote-wait +'set bufhidden=wipe'"
 else
   export VISUAL=nvim
 fi
